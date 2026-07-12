56d69a931b16b0755d31f94d4854ea58ce5c0302
Gaurav Jain <gauravrohinda0@gmail.com>
2026-07-12 16:20:35 +0530

    task2

---
+++ task2/variables.dart
@@ -0,0 +1,11 @@
+void main() {
+  String name = "Gaurav";
+  int age = 21;
+  double cgpa = 8.5;
+  bool isStudent = true;
+
+  print("Name: $name");
+  print("Age: $age");
+  print("CGPA: $cgpa");
+  print("Student: $isStudent");
+}
