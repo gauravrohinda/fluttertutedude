7fc7c850f92cc2c706883caa93f5c6a1f47b08b6
Gaurav Jain <gauravrohinda0@gmail.com>
2026-07-25 10:46:02 +0530

    task

---
+++ task4/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My Flutter App',
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+      ),
+      body: const Center(
+        child: Text(
+          "Welcome to My First Flutter App!",
+          style: TextStyle(fontSize: 22),
+        ),
+      ),
+    );
+  }
+}
